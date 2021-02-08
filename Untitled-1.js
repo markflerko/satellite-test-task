9c9bbc3a1d7d416cd4235f0875ad188fc4e3ccda
markflerko <markflerko@gmail.com>
2021-02-08 12:26:32 +0300

    init

---
+++ Untitled-1.js
@@ -0,0 +1,26 @@
+{
+  // /*
+  // 1. const f = () => {}  // -this
+  // 2. new f();
+  
+  // */
+  
+  // const arrow = () => {
+  //     // this.a = 1
+  //     return {}
+  // }
+  
+  // function f() {
+  //     // let obj = {} === this
+  
+  //     this.a = 1; // obj.a = 1
+  
+  //     this.funciton = () => {} // obj.funcition = () => {}
+  
+  //     return {}
+  
+  // }
+  
+  // console.log(new f())
+  }
+  
